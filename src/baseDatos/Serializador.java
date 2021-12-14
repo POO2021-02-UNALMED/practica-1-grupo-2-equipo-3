c193900a7c285fbd860aacdb7aa9486d78fd08e9
nsalinas350 <nsalinas@unal.edu.co>
2021-12-14 12:18:02 -0500

    Update Serializador.java

--- src/baseDatos/Serializador.java
+++ src/baseDatos/Serializador.java
@@ -1,5 +1,33 @@
 package baseDatos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+
+import gestorAplicacion.obras;
+
+
 public class Serializador {
+	private static File rutaTemp = new File("src\\baseDatos\\temp");
+	
+	
+	// Método para serializar todas las publicaciones creadas
+	public static void serializar(Publicacion publicacion) {}
+	
+	
+	
+	// Método para serializar todas las personas creadas
+
+	
+	
+	// Método para serializar todas las estanterias creadas
+
+	
+	
+	// Método para serializar todas los prestamos creados
+
 
 }
