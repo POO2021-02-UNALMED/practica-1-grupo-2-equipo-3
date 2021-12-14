0215f2c221dcc6a5b5da78c7b04b1ec7371fc193
nsalinas350 <nsalinas@unal.edu.co>
2021-12-14 14:28:39 -0500

    Update bibliotecario.java

--- src/uiMain/bibliotecario.java
+++ src/uiMain/bibliotecario.java
@@ -1,10 +1,60 @@
 package uiMain;
 
+import java.util.Scanner;
+
+import baseDatos.*; 
+import gestorAplicacion.obras.*;
+import gestorAplicacion.personas.*;
+import gestorAplicacion.prestamo.*;
+
+
 public class bibliotecario {
+	static Scanner sc = new Scanner(System.in);
+	static long readLong() {return sc.nextLong();}
+	static String readLn() {return sc.nextLine();}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Se almacenan en memoria ram todas las clases antes creadas
+		// Deserializador.deserializar();
+		
+		System.out.println("Hola!\nBienvenido al Sistema de Gestión de la Biblioteca");
+		
+		// Menú de opciones principal
+		int opcion;
+		do {
+			System.out.println("\nMenú de opciones: ");
+			System.out.println(" 1. Añadir Registros");
+			System.out.println(" 2. Mostrar registros");
+			
+			System.out.println("Teclee la opción: ");
+			opcion = (int) readLong();
+			
+			switch(opcion) {
+			case 1: System.out.println("Está resgistrando ..."); break;
+			case 2: System.out.println("Está mostrando ...");
+			case 5: break;
+			}
+			
+		} while (opcion !=5);
 
+	
+	
+	
+	
+	
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
