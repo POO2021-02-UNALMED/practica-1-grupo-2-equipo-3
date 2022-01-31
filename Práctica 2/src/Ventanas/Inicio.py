0e7bf49798ecdbd7252d2546022102c6d24b013e
nsalinas350 <nsalinas@unal.edu.co>
2022-01-31 11:49:30 -0500

    S

--- Práctica 2/src/Ventanas/Inicio.py
+++ Práctica 2/src/Ventanas/Inicio.py
@@ -21,9 +21,6 @@ if __name__ == "__main__":
     frameA = Frame(master=StartWindow)
     frameA.pack(side=LEFT)
 
-    frameB = Frame(master=StartWindow)
-    frameB.pack(side=RIGHT)
-
     #p3
     p3 = Frame(master=frameA,width=250,height=200,bg="red")
     p3.pack(side=TOP)
@@ -83,17 +80,110 @@ if __name__ == "__main__":
         p4.pack(side=BOTTOM) 
 
     
+
+    frameB = Frame(master=StartWindow)
     
     #p5
     p5 = Frame(master=frameB)
-    hojavida = Text(p5, font="Georgia",width=45,height=6)
-    hojavida.insert(INSERT,"Desarrollador:\n\n\
+    hojavida = Text(p5, font="Georgia",width=45,height=7)
+    hojavida.insert(1.0,"Desarrollador 1:\n\
+(Click en el texto para ver más desarrolladores)\n\n\
 Nombre:       Nelson Andrés Salinas Zapata\n\
 Profesión:     Estudiante de Pregrado\n\
 Carrera:        Ingeniería de Sistemas e Informática\n\
 Institución:  Universidad Nacional de Colombia sede Medellín")
     hojavida.pack()
     p5.pack(side=TOP)
+
+    frameB.pack(side=RIGHT)
+
+
+    #p6
+    p6 = Frame(master=frameB,width=250,height=400)
+
+    foto1 = PhotoImage(file="src/Ventanas/Imagenes/foto1.png")  
+    foto2 = PhotoImage(file="src/Ventanas/Imagenes/foto2.png")
+    foto3 = PhotoImage(file="src/Ventanas/Imagenes/foto3.png")
+    foto4 = PhotoImage(file="src/Ventanas/Imagenes/foto1.png")
+    foto5 = PhotoImage(file="src/Ventanas/Imagenes/foto5.png")
+    foto6 = PhotoImage(file="src/Ventanas/Imagenes/foto6.png")
+    foto7 = PhotoImage(file="src/Ventanas/Imagenes/foto7.png")
+    foto8 = PhotoImage(file="src/Ventanas/Imagenes/foto8.png")
+
+
+    framefoto1 = Frame(master=p6)
+    labelfoto1 = Label(framefoto1)
+    # foto1 = PhotoImage(file="src/Ventanas/Imagenes/foto1.png")
+    labelfoto1['image'] = foto1
+    labelfoto1.pack()
+    framefoto1.grid(row=1,column=1)
+
+    framefoto2 = Frame(master=p6)
+    labelfoto2 = Label(framefoto2)
+    # foto2 = PhotoImage(file="src/Ventanas/Imagenes/foto1.png")
+    labelfoto2['image'] = foto2
+    labelfoto2.pack()
+    framefoto2.grid(row=1,column=2)
+
+    framefoto3 = Frame(master=p6)
+    labelfoto3 = Label(framefoto3)
+    # foto3 = PhotoImage(file="src/Ventanas/Imagenes/foto1.png")
+    labelfoto3['image'] = foto3
+    labelfoto3.pack()
+    framefoto3.grid(row=2,column=1)
+
+    framefoto4 = Frame(master=p6)
+    labelfoto4 = Label(framefoto4)
+    # foto4 = PhotoImage(file="src/Ventanas/Imagenes/foto1.png")
+    labelfoto4['image'] = foto4
+    labelfoto4.pack()
+    framefoto4.grid(row=2,column=2)
+
+    p6.pack(side=BOTTOM) 
+
+
+    def frameB_1(e):
+        hojavida.delete(1.0,8.0)
+        hojavida.insert(1.0,"Desarrollador 1:\n\
+(Click en el texto para ver más desarrolladores)\n\n\
+Nombre:       Nelson Andrés Salinas Zapata\n\
+Profesión:     Estudiante de Pregrado\n\
+Carrera:        Ingeniería de Sistemas e Informática\n\
+Institución:  Universidad Nacional de Colombia sede Medellín")
+        hojavida.pack()
+
+        p6.pack_forget()
+        labelfoto1['image']= foto1
+        labelfoto2['image']= foto2
+        labelfoto3['image']= foto3
+        labelfoto4['image']= foto4
+        p6.pack(side=BOTTOM)
+
+        hojavida.bind('<ButtonPress-1>',frameB_2) 
+
+
+    def frameB_2(e):
+        hojavida.delete(1.0,8.0)
+        hojavida.insert(1.0,"Desarrollador 2:\n\
+(Click en el texto para ver más desarrolladores)\n\n\
+Nombre:       Luis Felipe Marín Buitrago\n\
+Profesión:     Estudiante de Pregrado\n\
+Carrera:        Ingeniería de Sistemas e Informática\n\
+Institución:  Universidad Nacional de Colombia sede Medellín")
+        hojavida.pack()
+
+        p6.pack_forget()
+        labelfoto1['image']= foto5
+        labelfoto2['image']= foto6
+        labelfoto3['image']= foto7
+        labelfoto4['image']= foto8
+        p6.pack(side=BOTTOM)
+
+        hojavida.bind('<ButtonPress-1>',frameB_1) 
+
+
+    #Bind inicial
+    hojavida.bind('<ButtonPress-1>',frameB_2)
 
     
 
