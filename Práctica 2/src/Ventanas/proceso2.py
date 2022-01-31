10f65692f6f50051c4ca1c9096febec90471debc
nsalinas350 <nsalinas@unal.edu.co>
2022-01-31 16:38:42 -0500

    Create proceso2.py

---
+++ Práctica 2/src/Ventanas/proceso2.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+class Frame2(Frame):
+    def __init__(self,w):
+        super().__init__(master=w,width=450)
+
+        nombre = Label(master=self,text="Proceso 2",
+        font=("Georgia",20)).pack()
+
+        desc = Label(master=self,text="Acá se pone una corta descripción de cada proceso"+
+        "\ningrese los correspondiente segín desee",
+        font=("Georgia",12)).pack()
+
+        interaccion = Frame(master=self)  # Frame de la zona de interacción
+        interaccion.pack()
