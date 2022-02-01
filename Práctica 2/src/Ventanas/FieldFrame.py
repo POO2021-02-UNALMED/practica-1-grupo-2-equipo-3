7db04950604f8e09209fb625d766bdfcde8d8920
nsalinas350 <nsalinas@unal.edu.co>
2022-02-01 09:31:33 -0500

    Update FieldFrame.py

--- Práctica 2/src/Ventanas/FieldFrame.py
+++ Práctica 2/src/Ventanas/FieldFrame.py
@@ -2,10 +2,14 @@ from re import T
 from tkinter import *
 
 class FieldFrame(Frame):
+    _criterios = []
+    _entrys = []
 
     def __init__(self,master,tituloCriterios, criterios, tituloValores,
         valores=None, habilitado=None):
         super().__init__(master,relief=GROOVE, borderwidth=4)
+        self._criterios = criterios
+        self._entrys = criterios.copy()
 
         Label(self,text=tituloCriterios).grid(row=0,column=0)
         Label(self,text=tituloValores).grid(row=0,column=1)
@@ -14,22 +18,26 @@ class FieldFrame(Frame):
             Label(self,text=criterios[i]).grid(row=i+1,column=0)
             if valores is not None:
                 if habilitado is None:
-                    e = Entry(self,width=50)
-                    e.grid(row=i+1,column=1)
-                    e.insert(0,valores[i])
+                    self._entrys[i] = Entry(self,width=50)
+                    self._entrys[i].grid(row=i+1,column=1)
+                    self._entrys[i].insert(0,valores[i])
                 else:
                     estado = NORMAL
                     if i+1 in habilitado:
                         estado = DISABLED
-                    e = Entry(self,width=50)
-                    e.grid(row=i+1,column=1)
-                    e.insert(0,valores[i])
-                    e.config(state=estado)
+                    self._entrys[i] = Entry(self,width=50)
+                    self._entrys[i].grid(row=i+1,column=1)
+                    self._entrys[i].insert(0,valores[i])
+                    self._entrys[i].config(state=estado)
             else:
-                Entry(self,width=50,textvariable=criterios[i]).grid(row=i+1,column=1)
+                self._entrys[i] = Entry(self,width=50)
+                self._entrys[i].grid(row=i+1,column=1)
 
-        def getValue(self,crit):
-            crit.get
+    def getValue(self,crit):
+        i = self._criterios.index(crit)
+        valor = StringVar() 
+        self._entrys[i].config(textvariable=valor)
+        return valor
 
 
 
