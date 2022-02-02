c931874f977265c05288de51359b4ba710ecd382
nsalinas350 <nsalinas@unal.edu.co>
2022-02-02 00:44:54 -0500

    Create m.py

---
+++ Práctica 2/src/m.py
@@ -0,0 +1,25 @@
+import pickle
+
+## Objetos
+from gestorAplicacion.obras.Estanteria import Estanteria
+from gestorAplicacion.obras.Folleto import Folleto
+from gestorAplicacion.obras.Libro import Libro
+from gestorAplicacion.obras.Publicacion import Publicacion
+from gestorAplicacion.obras.Revista import Revista
+from gestorAplicacion.personas.Autor import Autor
+from gestorAplicacion.personas.Usuario import Usuario
+##
+
+## Funciones
+from baseDatos.Almacenamiento import serializar,deserializar
+##
+
+
+
+serializar()
+
+deserializar() 
+
+
+# # Estanteria.setLista(pickle.load(r_estanterias))
+# r_estanterias.close()
